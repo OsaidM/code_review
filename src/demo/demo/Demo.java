1919db9b6d8f6fc62c315ae2c1dc91c1ffdf6bf4
OsaidM <osaid40@hotmail.com>
2024-09-07 10:39:17 +0300

    printing from 0 to 9

---
+++ src/demo/demo/Demo.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Working");
+		
+		for(int i=0; i< 10; i++) {
+			System.out.println(i);
+			
+		}
+	}
+
+}
